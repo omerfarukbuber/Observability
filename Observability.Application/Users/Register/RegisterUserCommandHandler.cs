0f0e34e802b88f00dba4cac7cedb5d2a34fd5a4c
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-01-05 12:22:16 +0300

    Handle the register operations in RegisterUserCommandHandler and use MassTransit's Guid Id generator

--- Observability.Application/Users/Register/RegisterUserCommandHandler.cs
+++ Observability.Application/Users/Register/RegisterUserCommandHandler.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using MassTransit;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Observability.Application.Abstractions.Authentication;
 using Observability.Application.Abstractions.CQRS;
 using Observability.Application.Abstractions.Data;
 using Observability.Domain.Users;
@@ -7,10 +9,10 @@ using Observability.Shared;
 
 namespace Observability.Application.Users.Register;
 
-public class RegisterUserCommandHandler(IApplicationDbContext context) : ICommandHandler<RegisterUserCommand, Guid>
+public class RegisterUserCommandHandler(
+    IApplicationDbContext context,
+    IPasswordHasher passwordHasher) : ICommandHandler<RegisterUserCommand, Guid>
 {
-    private readonly IApplicationDbContext _context = context;
-
     public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
         if (await context.Users.AnyAsync(u => u.Email.Equals(request.Email), cancellationToken))
@@ -18,8 +20,18 @@ public class RegisterUserCommandHandler(IApplicationDbContext context) : IComman
             return Result<Guid>.Failure(Error.Conflict("User.EmailNotUnique", "User email is not unique."));
         }
 
-        
-        
-        return Result<Guid>.Success(Guid.NewGuid());
+        var user = new User
+        {
+            UserId = NewId.NextSequentialGuid(),
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            PasswordHash = passwordHasher.Hash(request.Password)
+        };
+
+        await context.Users.AddAsync(user, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result<Guid>.Success(user.UserId);
     }
 }
