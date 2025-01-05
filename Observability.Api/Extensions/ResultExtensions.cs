1b1fc62352db25634becbea78483ed7284dcc179
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-01-05 12:03:38 +0300

    Create Extension method to Result object for separate return types which can be successful or failure

---
+++ Observability.Api/Extensions/ResultExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Observability.Api.Infrastructure;
+using Observability.Shared;
+
+namespace Observability.Api.Extensions;
+
+internal static class ResultExtensions
+{
+    public static TOut Match<TOut>(
+        this Result result,
+        Func<TOut> onSuccess,
+        Func<Result, TOut> onFailure)
+    {
+        return result.IsSuccess
+            ? onSuccess()
+            : onFailure(result);
+    }
+
+    public static TOut Match<TIn, TOut>(
+        this Result<TIn> result,
+        Func<TIn, TOut> onSuccess,
+        Func<Result<TIn>, TOut> onFailure)
+    {
+        return result.IsSuccess
+            ? onSuccess(result.Value)
+            : onFailure(result);
+    }
+
+    public static void ConfigureCustomResult(this IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        CustomResults.Configure(serviceScope.ServiceProvider.GetRequiredService<IHttpContextAccessor>());
+    }
+}
