ae6d911ca2c186eed012865b4079b2088f6f5001
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2025-01-05 12:00:47 +0300

    Improve Result and Error Objects in Shared
    -Change Error Property accessor in the Result Class.
    -Create ValidationError record for seperate validation errors from another error types.
    -Change constructor access modifier in Error record.

---
+++ Observability.Shared/ValidationError.cs
@@ -0,0 +1,13 @@
+﻿namespace Observability.Shared;
+
+public sealed record ValidationError : Error
+{
+    public ValidationError(Error[] errors) : base("Validation.General",
+        "One or more validation errors occured.",
+        ErrorType.Validation)
+    {
+        Errors = errors;
+    }
+
+    public Error[] Errors { get; }
+}
